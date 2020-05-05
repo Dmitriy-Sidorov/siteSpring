583d958fcc116f22f2b9d726f55351f24f4bdaa0
Dmitriy Sidorov <dmitrii.sidorov@accenture.com>
2020-05-05 15:09:27 +0300

    create fragments

--- src/main/java/com/siteSpring/blog/controllers/MainController.java
+++ src/main/java/com/siteSpring/blog/controllers/MainController.java
@@ -1,6 +1,5 @@
 package com.sitespring.blog.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
