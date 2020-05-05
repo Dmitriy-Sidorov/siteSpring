0ab09ad0dd9fc9606dd3250d5801fc6c524def22
Dmitriy Sidorov <dmitrii.sidorov@accenture.com>
2020-05-05 13:51:04 +0300

    main page

---
+++ src/main/java/com/siteSpring/blog/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.sitespring.blog.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "mainPage";
+    }
+}
