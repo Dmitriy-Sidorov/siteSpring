78a7eb3355b8a0063958c88b3f2f2b832d7a7518
Dmitriy Sidorov <dmitrii.sidorov@accenture.com>
2020-05-05 18:59:04 +0300

    added create post

--- src/main/java/com/siteSpring/blog/controllers/BlogController.java
+++ src/main/java/com/siteSpring/blog/controllers/BlogController.java
@@ -1,14 +1,36 @@
 package com.sitespring.blog.controllers;
 
+import com.sitespring.blog.models.Post;
+import com.sitespring.blog.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
-    public String mainPage(Model model) {
+    public String blogPage(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blogPage";
+    }
+
+    @GetMapping("/blog/new-post")
+    public String newPostPage(Model model) {
+        return "newPostPage";
+    }
+
+    @PostMapping("/blog/new-post")
+    public String newPost(@RequestParam String title, @RequestParam String anons, @RequestParam String article, Model model) {
+        Post post = new Post(title, anons, article);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 }
